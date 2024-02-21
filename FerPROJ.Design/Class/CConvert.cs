e881cfd4723d3b119bb3c78b49f42136297035bf
Feric Decenan <feric08c@gmail.com>
2024-02-21 21:31:56 +0800

    added

--- FerPROJ.Design/Class/CConvert.cs
+++ FerPROJ.Design/Class/CConvert.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,8 +9,11 @@ using System.Windows.Forms;
 namespace FerPROJ.Design.Class {
     public static class CConvert {
 
-        public static string GetDate(DateTime dtp) {
+        public static string GetDate(this DateTime dtp) {
             return dtp.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        public static TEnum GetEnum<TEnum>(this string text) where TEnum : Enum {
+            return (TEnum)Enum.Parse(typeof(TEnum), text);
         }
     }
 }

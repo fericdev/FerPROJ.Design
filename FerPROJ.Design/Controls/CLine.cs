ce5addcdee58a664e140cbd8261280266f58fe4e
Feric Decenan <feric08c@gmail.com>
2024-03-17 09:14:24 +0800

    new

---
+++ FerPROJ.Design/Controls/CLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FerPROJ.Design.Controls {
+    public class CLine : Label {
+        public CLine()
+        {
+            this.BorderStyle = BorderStyle.Fixed3D;
+            this.AutoSize = false;
+            this.Height = 3; 
+            this.Width = 50;
+            this.Text = "";
+        }
+    }
+}

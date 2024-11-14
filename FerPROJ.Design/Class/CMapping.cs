1aac6e68a37af90e2a4dbae889a2d34433c33e41
fericqt <feric08c@gmail.com>
2024-11-14 08:56:51 +0800

    added static method for mapping

--- FerPROJ.Design/Class/CMapping.cs
+++ FerPROJ.Design/Class/CMapping.cs
@@ -1,10 +1,56 @@
 ﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
 
-namespace FerPROJ.Design.Class
-{
-    public class CMapping<TSource, TDestination> : Profile 
-    {
+namespace FerPROJ.Design.Class {
+    public static class CMappingExtension {
+
+        public static TDestination ToDestination<TDestination>(this object source) {
+            if (source == null) {
+                return default;
+            }
+
+            // Use reflection to create the mapping instance dynamically
+            var sourceType = source.GetType();
+            var mappingType = typeof(CMapping<,>).MakeGenericType(sourceType, typeof(TDestination));
+            var mappingInstance = Activator.CreateInstance(mappingType);
+
+            // Call the 'GetMappingResult' method on the mapping instance
+            var getMappingResultMethod = mappingType.GetMethod("GetMappingResult");
+            return (TDestination)getMappingResultMethod.Invoke(mappingInstance, new object[] { source });
+        }
+
+        public static List<TDestination> ToDestination<TDestination>(this IEnumerable<object> source) {
+            if (source == null || !source.Any()) {
+                return default;
+            }
+            var resultList = new List<TDestination>();
+            foreach (var item in source) {
+                var map = item.ToDestination<TDestination>();
+                resultList.Add(map);
+            }
+            return resultList;
+        }
+
+        public static List<TDestination> ToDestination<TDestination>(this ICollection<object> source) {
+            if (source == null || source.Count == 0) {
+                return default;
+            }
+            var resultList = new List<TDestination>();
+            foreach (var item in source) {
+                var map = item.ToDestination<TDestination>();
+                resultList.Add(map);
+            }
+            return resultList;
+        }
+
+    }
+
+    public class CMapping<TSource, TDestination> : Profile {
         public CMapping() {
             CreateMap<TSource, TDestination>().ReverseMap();
         }
@@ -18,8 +64,7 @@ namespace FerPROJ.Design.Class
             return mapper.Map<TSource, TDestination>(myDTO);
         }
     }
-    public class CMappingList<TSource, TDestination>
-    {
+    public class CMappingList<TSource, TDestination> {
         public List<TDestination> GetMappingResultList(ICollection<TSource> source) {
             //
             List<TDestination> resultList = new List<TDestination>();

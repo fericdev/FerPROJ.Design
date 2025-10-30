a69b2d412c28211cd7a8c45e6af1b2fba472d6e2
Feric Decenan <feric.decenan.git@gmail.com>
2025-10-30 21:36:30 +0800

    yes

--- FerPROJ.Design/Class/CNotificationManager.cs
+++ FerPROJ.Design/Class/CNotificationManager.cs
@@ -8,13 +8,13 @@ using System.Windows.Forms;
 
 namespace FerPROJ.Design.Class {
     public static class CNotificationManager {
-        public static async Task CreateNotificationAndDisplayAsync(NotificationDTO notification, Func<Task> runTaskWhenClick) {
-            if(notification == null) {
+        public static async Task CreateNotificationAndDisplayAsync(NotificationModel notification, Func<Task> runTaskWhenClick) {
+            if (notification == null) {
                 return;
             }
             await DisplayNotification(notification, runTaskWhenClick);
         }
-        private static async Task DisplayNotification(NotificationDTO notification, Func<Task> action) {
+        private static async Task DisplayNotification(NotificationModel notification, Func<Task> action) {
             var notifyIcon = new NotifyIcon();
             //
             notifyIcon.Icon = new Icon(SystemIcons.Information, 50, 50);
@@ -25,10 +25,11 @@ namespace FerPROJ.Design.Class {
                 await action();
             };
             //
-            if (notification.IntervalTimeSeconds <= 0) {
+            if (notification.DelayTimeSeconds > 0) {
+                await Task.Delay(notification.DelayTimeSeconds * 1000);
                 await ShowBalloonTip();
             }
-            else {
+            if (notification.IntervalTimeSeconds > 0) {
                 var timer = new Timer { Interval = notification.IntervalTimeSeconds * 1000 };
                 timer.Tick += async (sender, e) => {
                     await ShowBalloonTip();
@@ -44,10 +45,11 @@ namespace FerPROJ.Design.Class {
         }
 
     }
-    public class NotificationDTO {
-        public string Title { get; set; }   
+    public class NotificationModel {
+        public string Title { get; set; }
         public string Description { get; set; }
         public int DisplayTimeSeconds { get; set; }
+        public int DelayTimeSeconds { get; set; }
         public int IntervalTimeSeconds { get; set; }
     }
 }

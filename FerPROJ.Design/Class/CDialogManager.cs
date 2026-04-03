b67683f4e7591aa3b605a0e35bada599417d18f0
Feric Decenan <feric.decenan.git@gmail.com>
2026-04-03 08:09:57 +0800

    yes

--- FerPROJ.Design/Class/CDialogManager.cs
+++ FerPROJ.Design/Class/CDialogManager.cs
@@ -1,45 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FerPROJ.Design.Class
-{
-    public static class CDialogManager
-    {
-        public static bool Ask(string message, string caption)
-        {
-            if(MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
+namespace FerPROJ.Design.Class {
+    public static class CDialogManager {
+        public static bool Ask(string message, string caption) {
+            if (MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
                 return true;
             }
             return false;
         }
         public static bool Ask(string message) {
-            if (MessageBox.Show(message, CAssembly.SystemName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
-                return true;
-            }
-            return false;
+            var callerType = new StackTrace().GetFrame(1).GetMethod().DeclaringType;
+            return Ask(message, GetCallerTypeName(callerType));
         }
-        public static void Warning(string message, string caption)
-        {
+        public static void Warning(string message, string caption) {
             MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
         public static void Warning(string message) {
-            MessageBox.Show(message, CAssembly.SystemName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            var callerType = new StackTrace().GetFrame(1).GetMethod().DeclaringType;
+            Warning(message, GetCallerTypeName(callerType));
         }
-        public static void Info(string message, string caption)
-        {
+        public static void Info(string message, string caption) {
             MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
         public static void Info(string message) {
-            MessageBox.Show(message, CAssembly.SystemName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var callerType = new StackTrace().GetFrame(1).GetMethod().DeclaringType;
+            Info(message, GetCallerTypeName(callerType));
         }
-        public static void Create(string message, string caption, MessageBoxIcon msgIcon)
-        {
+        public static void Custom(string message, string caption, MessageBoxIcon msgIcon) {
             MessageBox.Show(message, caption, MessageBoxButtons.OK, msgIcon);
+        }
+
+
+        private static string GetCallerTypeName(Type type) {
+            if (type == null) {
+                return CAssembly.SystemName;
+            }
+
+            // Remove compiler async parts first
+            var cleaned = Regex.Replace(type?.Name, @"\<.*\>d__\d+", "");
+
+            // Remove any non-alphanumeric character (keep _ if desired)
+            cleaned = Regex.Replace(cleaned, @"[^a-zA-Z0-9_]", "");
+
+            return cleaned;
         }
     }
 }

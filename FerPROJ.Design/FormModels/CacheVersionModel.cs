dcd6ee5d10038658580e6f98fb19aa7dc16947f9
Feric Decenan <feric.decenan.git@gmail.com>
2026-05-15 08:53:17 +0800

    yes

---
+++ FerPROJ.Design/FormModels/CacheVersionModel.cs
@@ -0,0 +1,12 @@
+﻿using FerPROJ.Design.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerPROJ.Design.FormModels {
+    public class CacheVersionModel : BaseModel {
+        public int VersionNo { get; set; }
+    }
+}

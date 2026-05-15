88bfbad2880381c7092d8804367a3efc0d510da7
Feric Decenan <feric.decenan.git@gmail.com>
2026-05-16 07:59:54 +0800

    removed crystal report and references

--- FerPROJ.Design/Controls/CComboBoxKrypton.cs
+++ FerPROJ.Design/Controls/CComboBoxKrypton.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CrystalDecisions.CrystalReports.ViewerObjectModel;
 using Krypton.Toolkit;
 
 namespace FerPROJ.Design.Controls {

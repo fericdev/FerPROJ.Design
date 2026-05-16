944b766300a6b49e203b12528ec2c53f95a3bf83
Feric Decenan <feric.decenan.git@gmail.com>
2026-05-16 15:52:15 +0800

    yes

--- FerPROJ.Design/BaseModels/BaseModel.cs
+++ FerPROJ.Design/BaseModels/BaseModel.cs
@@ -41,6 +41,7 @@ namespace FerPROJ.Design.BaseModels {
         public virtual string Status { get; set; } = CAppConstants.ACTIVE_STATUS;
         [CAttributes(Visible = true, Order = 2001)]
         public virtual string Remarks { get; set; }
+        public string ApplicationId { get; set; } = CAppConstants.APPLLICATION_ID;
         public virtual void Execute() {
             
         }

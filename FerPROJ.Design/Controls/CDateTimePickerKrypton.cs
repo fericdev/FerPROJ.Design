b1aa692c5b73a9595cefc94681a2a0d0b6a60332
Feric Decenan <feric.decenan.git@gmail.com>
2026-05-20 08:11:46 +0800

    Include interest to project

--- FerPROJ.Design/Controls/CDateTimePickerKrypton.cs
+++ FerPROJ.Design/Controls/CDateTimePickerKrypton.cs
@@ -82,4 +82,41 @@ namespace FerPROJ.Design.Controls {
             StateCommon.Content.Color1 = foreColor;
         }
     }
+    public class CDatePickerKrypton : KryptonDateTimePicker {
+        private Color borderColor = Color.White;
+        private Color foreColor = Color.Black;
+
+        public CDatePickerKrypton() {
+            StateIsActive();
+            StateIsCommon();
+            StateIsNormal();
+            Format = DateTimePickerFormat.Custom;
+            CustomFormat = "MM/dd/yyyy";
+            ShowUpDown = false;
+        }
+        private void StateIsActive() {
+
+            StateActive.Border.Color1 = Color.DarkGray;
+            StateActive.Border.Color2 = borderColor;
+            //
+            StateActive.Border.Rounding = 10;
+            StateActive.Content.Color1 = foreColor;
+        }
+        private void StateIsCommon() {
+
+            StateCommon.Border.Color1 = Color.DarkGray;
+            StateCommon.Border.Color2 = borderColor;
+            //
+            StateCommon.Border.Rounding = 10;
+            StateCommon.Content.Color1 = foreColor;
+        }
+        private void StateIsNormal() {
+
+            StateNormal.Border.Color1 = Color.DarkGray;
+            StateNormal.Border.Color2 = borderColor;
+            //
+            StateCommon.Border.Rounding = 10;
+            StateCommon.Content.Color1 = foreColor;
+        }
+    }
 }

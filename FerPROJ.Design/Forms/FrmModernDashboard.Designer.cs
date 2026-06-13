69e03c7d6893610aee2bdbd7426e3002a0ea185c
Feric Decenan <feric.decenan.git@gmail.com>
2026-06-13 09:33:36 +0800

    yes

---
+++ FerPROJ.Design/Forms/FrmModernDashboard.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace FerPROJ.Design.Forms {
+    partial class FrmModernDashboard {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            ((System.ComponentModel.ISupportInitialize)(this.baseDashboardKryptonPanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baseKryptonPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMainTop
+            // 
+            this.pnlMainTop.Size = new System.Drawing.Size(1661, 96);
+            // 
+            // pnlMainBot
+            // 
+            this.pnlMainBot.Location = new System.Drawing.Point(0, 732);
+            this.pnlMainBot.Size = new System.Drawing.Size(1661, 30);
+            // 
+            // baseDashboardKryptonPanel
+            // 
+            this.baseDashboardKryptonPanel.Size = new System.Drawing.Size(1661, 611);
+            // 
+            // baseKryptonPanel
+            // 
+            this.baseKryptonPanel.Size = new System.Drawing.Size(1661, 762);
+            // 
+            // FrmModernDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1661, 762);
+            this.Name = "FrmModernDashboard";
+            this.StateCommon.Back.Color1 = System.Drawing.Color.RoyalBlue;
+            this.StateCommon.Back.Color2 = System.Drawing.Color.RoyalBlue;
+            this.StateCommon.Border.Color1 = System.Drawing.Color.RoyalBlue;
+            this.StateCommon.Border.Color2 = System.Drawing.Color.RoyalBlue;
+            this.StateCommon.Header.Back.Color2 = System.Drawing.Color.RoyalBlue;
+            this.StateCommon.Header.Border.Color1 = System.Drawing.Color.RoyalBlue;
+            this.StateCommon.Header.Border.Color2 = System.Drawing.Color.RoyalBlue;
+            this.Text = "FrmModernDashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.baseDashboardKryptonPanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baseKryptonPanel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
